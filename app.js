ca7c82a370c1d222805a4b5a2cdccea05905f1b5
siascone <spencer.iascone@gmail.com>
2021-06-15 16:16:10 -0600

    Setup User Auth

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+// Routes
+const users = require('./routes/api/users');
+const cheets = require('./routes/api/cheets');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB suvvessfully"))
     .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send("Hello World"));
+app.use('/api/users', users);
+app.use('/api/cheets', cheets);
+
 
 const port = process.env.PORT || 5000;
 
